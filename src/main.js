f4e559e654fb5299e7433702c92a118799e9227d
chinomso okoro <johnkennedychinomso@gmail.com>
2026-01-18 19:23:04 +0100

    vite

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ function addProductToDOM() {
   }" class="w-full h-50 object-cover mb-2 lg:h-60 md:h-60"/>
 <h2 class="text-sm font-semibold mb-2">${product.name}</h2>
   <p class="text-sm font-semibold text-blue-600">$${product.price.toFixed(
-    2
+    2,
   )}</p>
   <button class="mt-4 bg-black text-white text-xs px-4 py-2 rounded w-full hover:bg-white hover:text-black">Add to Cart</button>
   <button class="mt-2 bg-green-500 text-white text-xs px-4 py-2 rounded w-full hover:bg-green-600">Delete</button>
@@ -32,3 +32,6 @@ const addBtn = document.getElementById("add-product");
 addBtn.addEventListener("click", () => {
   addProductToDOM();
 });
+
+const shopImg = document.getElementById("shop");
+shopImg.src = product.productImage;
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/shopwill store/",
+  base: ".",
   plugins: [tailwindcss()],
 });
