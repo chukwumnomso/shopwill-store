84f89c68b344a2f8e6de95b9341969741406d928
chinomso okoro <johnkennedychinomso@gmail.com>
2026-01-21 18:31:10 +0100

    hero-bg changed for test

--- src/prodUpload.js
+++ src/prodUpload.js
@@ -25,7 +25,6 @@ productImg.addEventListener("change", (e) => {
 
   const file = e.target.files[0];
   uploadproduct(file);
-  // loadImage();
   console.log(file);
 });
 
@@ -34,7 +33,7 @@ export async function uploadproduct(file) {
   const filePath = `productImage/${Date.now()}-${avatar}`;
   const { data, error } = await supabase.storage
     .from("productImage")
-    .upload(filePath, file);
+    .upload(filePath, file, { cacheControl: "3600", upsersert: true });
 
   prodName();
   price();
@@ -42,14 +41,12 @@ export async function uploadproduct(file) {
   try {
     const { data: insertData } = await supabase
       .from("products")
-      .insert([
-        {
-          product_name: prodName(),
-          product_price: price(),
-          product_image: filePath,
-          product_id: prodId(),
-        },
-      ])
+      .upsert({
+        product_name: prodName(),
+        product_price: price(),
+        product_image: filePath,
+        id: prodId(),
+      })
       .select()
       .single();
   } catch {}
@@ -69,25 +66,35 @@ export async function renderProduct() {
 
     const prodcard = document.createElement("div");
     prodcard.innerHTML = `<div
-        class="bg-white shadow-md shadow-gray-400 rounded-xl py-4 px-4 capitalize relative "
+        class="bg-white shadow-md shadow-gray-300 rounded-xl py-4 px-4 capitalize relative h-80 grid grid-rows-2 sm:h-100 md:h-120 "
       >
-<div class=" items-center justify-center text-sm font-[outfit] text-white bg-red-600 mt-3 absolute top-0 left-0  h-5 w-15 hidden"
+<div class=" items-center justify-center text-sm font-[outfit] text-white bg-red-600 mt-3 absolute top-0 left-0  h-5 w-15 text-center z-10 sold" 
       >sold</div>
-        <img src="${prodImage}" class=" hover:scale-101 transition duration-300 overflow-hidden" />
-         <div class="flex justify-between items-center "><h2 class="font-bold font-[outfit] text-sm mb-2 mt-2">${card.product_name}</h2><div
+       <div class=" flex items-center justify-center overflow-hidden row-span-3 "> <img src="${prodImage}" class=" hover:scale-101 transition h-full w-full duration-300 overflow-hidden" /></div>
+
+        <div class="flex-col items-center justify-center "> <div class="flex justify-between items-center "><h2 class="font-bold font-[outfit] text-sm mb-2 mt-2">${card.product_name}</h2><div
       class="size-6 bg-purple-600 flex justify-center items-center rounded-full hover:bg-purple-700 cursor-pointer"
     >  <i class="fa-solid fa-bag-shopping text-sm text-white font-light"></i></div></div>
         
         <p class="text-[0.8rem] text-black font-semibold">₦${card.product_price}</p>
-         <p class="text-[0.7rem] text-pink-500 hidden">${card.product_id}</p>
-         <button class="w-full h-8 bg-black text-white font-semibold flex items-center justify-center rounded-lg mt-2 hover:bg-gray-800 cursor-pointer">view</button>
-      </div>`;
+         <p class="text-[0.7rem] text-pink-500 hidden">${card.id}</p>
+         <button class="w-full h-8 bg-black text-white font-semibold flex items-center justify-center mt-2 hover:bg-gray-800 transition-all duration-300 cursor-pointer">view</button>
+      </div></div>
+        `;
 
     prodList.appendChild(prodcard);
+    return prodcard;
   });
 }
 
-renderProduct();
+renderProduct()
+  .then((get) => {
+    const solds = document.querySelectorAll(".sold");
+    solds.forEach((sold) => {
+      sold.classList.add("hidden");
+    });
+  })
+  .catch((error) => console.error(error));
 
 // const prodcard = document.createElement("div");
 // prodcard.innerHTML = `<div
