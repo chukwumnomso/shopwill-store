a3e5bc8757011cac746d66853538c32287116d77
chinomso okoro <johnkennedychinomso@gmail.com>
2026-01-27 16:39:26 +0100

    base changed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/",
+  base: "/shopwill-store/",
   plugins: [tailwindcss()],
 });
