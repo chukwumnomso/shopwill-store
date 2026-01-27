f01192d59466ab62ff909e624843cfb22a306931
chinomso okoro <johnkennedychinomso@gmail.com>
2026-01-27 17:32:23 +0100

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 // vite.config.js
 import { defineConfig } from "vite";
+import { resolve } from "path";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   base: "/shopwill-store/",
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        products: resolve(__dirname, "products.html"),
+      },
+    },
+  },
 });
